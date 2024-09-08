7eb92c847315bf5788876eb3c36ce70391f8f7ca
Ibon Azkoitia <ibon@kreatidos.com>
2024-09-08 11:49:25 +0200

    Customize the Contacs Form

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\ContactResource\Pages;
 use App\Filament\Resources\ContactResource\RelationManagers;
 use App\Models\Contact;
 use Filament\Forms;
+use Filament\Forms\Components\Wizard;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -29,29 +30,59 @@ class ContactResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('first_name'),
-                Forms\Components\TextInput::make('last_name'),
-                Forms\Components\TextInput::make('email')
-                    ->email(),
-                Forms\Components\TextInput::make('phone')
-                    ->tel(),
-                Forms\Components\DatePicker::make('birthday'),
-                Forms\Components\Textarea::make('description')
-                    ->columnSpanFull(),
-                Forms\Components\FileUpload::make('image')
-                    ->image(),
-                Forms\Components\TextInput::make('job_title'),
-                Forms\Components\TextInput::make('source_id')
-                    ->numeric(),
-                Forms\Components\TextInput::make('url_linkedin'),
-                Forms\Components\TextInput::make('url_website'),
-                Forms\Components\TextInput::make('url_x'),
-                Forms\Components\TextInput::make('street'),
-                Forms\Components\TextInput::make('city'),
-                Forms\Components\TextInput::make('state'),
-                Forms\Components\TextInput::make('postcode'),
-                Forms\Components\TextInput::make('country'),
-            ]);
+                Wizard::make([
+                    Wizard\Step::make('Info')
+                        ->columns(2)
+                        ->schema([
+                            Forms\Components\FileUpload::make('image')
+                                ->label('Photo')
+                                ->directory('contacts')
+                                ->preserveFilenames()
+                                ->avatar()
+                                ->imageEditor()
+                                ->circleCropper()
+                                ->maxSize(1024)
+                                ->columnSpan(2),
+                            Forms\Components\TextInput::make('first_name'),
+                            Forms\Components\TextInput::make('last_name'),
+                            Forms\Components\TextInput::make('email')
+                                ->email(),
+                            Forms\Components\TextInput::make('phone'),
+                            Forms\Components\DatePicker::make('birthday'),
+                            Forms\Components\Textarea::make('description'),
+                            Forms\Components\TextInput::make('job_title'),
+                            Forms\Components\Select::make('source_id')
+                                ->relationship('source', 'name')
+                        ]),
+                    Wizard\Step::make('Social Media')
+                        ->columns(2)
+                        ->schema([
+                            Forms\Components\TextInput::make('url_linkedin')
+                                ->label('LinkedIn')
+                                ->url()
+                                ->placeholder('https://linkedin.com/'),
+                            Forms\Components\TextInput::make('url_website')
+                                ->label('Website')
+                                ->url()
+                                ->placeholder('https://prolinks.pro/'),
+                            Forms\Components\TextInput::make('url_x')
+                                ->label('X')
+                                ->url()
+                                ->placeholder('https://x.com/'),
+                        ]),
+                    Wizard\Step::make('Address')
+                        ->columns(2)
+                        ->schema([
+                            Forms\Components\TextInput::make('street'),
+                            Forms\Components\TextInput::make('city'),
+                            Forms\Components\TextInput::make('state'),
+                            Forms\Components\TextInput::make('postcode'),
+                            Forms\Components\TextInput::make('country'),
+                        ]),
+                ])
+                ->columnSpan(3),
+            ])
+            ->columns(4);
     }
 
     public static function table(Table $table): Table
